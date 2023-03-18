5fc7fc723f7860cc202cded8f942f8cc5c798762
Aziz Can HAMASOGLU <azizcanhamas@gmail.com>
2023-03-18 16:29:36 +0300

    first update.

---
+++ codes/arduino/u8glib_oled/oled_arduino.ino
@@ -0,0 +1,28 @@
+#include "U8glib.h"
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0);
+char bufferX [20];
+char bufferY [20];
+char bufferZ [20];
+void setup() {
+
+}
+
+void loop() {
+  sprintf(bufferX, "    Aziz Can");
+  sprintf(bufferY, "     OLED");
+  u8g.firstPage();
+  do {
+    draw();
+  } while ( u8g.nextPage() );
+  delay(10);
+}
+
+void draw(void)
+{
+
+
+  u8g.setFont(u8g_font_unifont);
+  u8g.drawStr( 0, 20, bufferX);
+  u8g.drawStr( 0, 40, bufferY);
+  u8g.drawStr( 0, 60, bufferZ);
+}
