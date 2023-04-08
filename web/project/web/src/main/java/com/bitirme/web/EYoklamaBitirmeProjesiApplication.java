f3dd1794d0a0fc49ee201b7694c9442dd60424ea
Aziz Can HAMASOGLU <azizcanhamas@gmail.com>
2023-04-08 15:13:47 +0300

    web design added.

---
+++ web/project/web/src/main/java/com/bitirme/web/EYoklamaBitirmeProjesiApplication.java
@@ -0,0 +1,13 @@
+package com.bitirme.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EYoklamaBitirmeProjesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EYoklamaBitirmeProjesiApplication.class, args);
+	}
+
+}
