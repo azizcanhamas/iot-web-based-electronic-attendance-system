81d135831dcf688258cd260154db062dcdcbc5e7
Aziz Can HAMASOGLU <azizcanhamas@gmail.com>
2023-04-15 16:47:15 +0300

    plan updated.

---
+++ web/project/web/src/main/java/com/bitirme/web/Controller/APIController/DeviceAPI.java
@@ -0,0 +1,11 @@
+package com.bitirme.web.Controller.APIController;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class DeviceAPI {
+
+    
+
+}
